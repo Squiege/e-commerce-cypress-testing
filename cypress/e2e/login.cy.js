c701721b6b6130ebd0ff2cacc38c5c961bf7ac19
Devin Rider <riderdevin@yahoo.com>
2025-05-10 13:06:02 -0700

    Login Tests, correct error message validation, singout function test, html5 popup test refactor

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,34 +1,73 @@
-describe('Salinaka E-Commerce Login', () => {
+describe('Salinaka E-Commerce Login Tests', () => {
 
-    // Successful Login Test
     it('Logs in successfully with valid credentials', () => {
-        cy.visit('https://salinaka-ecommerce.web.app') 
-        cy.contains('Sign In').click() 
+        cy.visit('https://salinaka-ecommerce.web.app')
 
+      // Click Sign In button
+        cy.contains('Sign In').click()
+
+      // Fill in the login form
         cy.get('#email').type('devintest@gmail.com')
         cy.get('#password').type('Deblintest123')
         cy.get('.auth-action > .button').click()
+
+      // Check URL after redirect
         cy.url().should('include', '/')
+        cy.contains('Shop').should('be.visible')
     })
 
-    // Unsuccessful Login Test
     it('Fails to login with invalid credentials', () => {
-        cy.visit('https://salinaka-ecommerce.web.app') 
-        cy.contains('Sign In').click() 
+        cy.visit('https://salinaka-ecommerce.web.app')
 
+        // Click Sign In button
+        cy.contains('Sign In').click()
+
+        // Fill in the login form with invalid credentials
         cy.get('#email').type('wrong_user')
         cy.get('#password').type('wrong_pass')
         cy.get('.auth-action > .button').click()
-        cy.get('.auth-action > .button').should('be.visible')
+
+        // Check that error message is shown
+        cy.get('#email').then(($input) => {
+            expect($input[0].checkValidity()).to.be.false
+            expect($input[0].validationMessage).to.include('@')
+        })
     })
 
-    // Empty Form Submission Test
     it('Shows error for empty form submission', () => {
-        cy.visit('https://salinaka-ecommerce.web.app') 
-        cy.contains('Sign In').click() 
-
+        cy.visit('https://salinaka-ecommerce.web.app')
+        
+        // Click Sign In button
+        cy.contains('Sign In').click()
+    
+        // Submit with no input
         cy.get('.auth-action > .button').click()
-        cy.get('.auth-action > .button').should('be.visible')
+    
+        // Check that error label is shown
+        cy.get('.label-error')
+            .should('be.visible')
+            .and('contain.text', 'Email is required.')
+        })
+
+    it('Logs out after successful login', () => {
+        cy.visit('https://salinaka-ecommerce.web.app')
+
+        // Click Sign In button
+        cy.contains('Sign In').click()
+
+        // Fill in the login form
+        cy.get('#email').type('devintest@gmail.com')
+        cy.get('#password').type('Deblintest123')
+        cy.get('.auth-action > .button').click()
+
+        // Click on User dropdown
+        cy.get('.anticon.anticon-down').click()
+        // Sign out of the account
+        cy.contains('Sign Out').click()
+
+        // Check URL after redirect
+        cy.url().should('include', '/signin')
+        cy.contains('Sign In').should('be.visible')
     })
 
-})
+    })
--- cypress/e2e/product.cy.js
+++ cypress/e2e/product.cy.js
@@ -1,96 +1,55 @@
 describe('Salinaka E-Commerce Product Tests', () => {
-    it('Checks if the product grid, names, price, images, and name are visible', () => {
-        cy.visit('https://salinaka-ecommerce.web.app')
-
-        // Click Shop
-        cy.contains('Shop').click()
-
-        // Wait for products to load and checks if they are visible
-        cy.get('.product-card', { timeout: 10000 }).should('be.visible')
-
-        // Check if all the cards contain a product names 
-        cy.get('.product-card > .product-card-content > .product-details > .product-card-name').should('exist')
-        
-        // Check if all the cards contain a product price
-        cy.get('.product-card > .product-card-content > .product-details > .product-card-price').should('exist')
-
-        // Check if all the cards contain a product image
-        cy.get('.product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').should('exist')
-
-        // Check if all the cards contain a brand name
-        cy.get('.product-card > .product-card-content > .product-details > .product-card-brand').should('exist')
+    it('Checks product grid elements (name, brand, price, image)', () => {
+      cy.visit('https://salinaka-ecommerce.web.app')
+      cy.contains('Shop').click()
+  
+      cy.get('.product-card', { timeout: 10000 }).should('have.length.at.least', 1)
+  
+      cy.get('.product-card').each(($card) => {
+        cy.wrap($card).within(() => {
+          cy.get('.product-card-name').should('not.be.empty')
+          cy.get('.product-card-brand').should('not.be.empty')
+          cy.get('.product-card-price').should('contain.text', '$')
+          cy.get('.product-card-img').should('have.attr', 'src')
+        })
+      })
     })
-
-    it('Checks if product hover functionality works', () => {
-        cy.visit('https://salinaka-ecommerce.web.app')
-        // Click Shop
-        cy.contains('Shop').click()
-
-        // Wait for products to load and checks if they are visible
-        cy.get('.product-card', { timeout: 10000 }).should('be.visible')
-
-        // Hover over the first product card
-        cy.get(':nth-child(1) > .product-card').trigger('mouseover')
-
-        // Check if the hover image is visible
-        cy.get(':nth-child(1) > .product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').should('be.visible')
-
-        // Check if the hover button is visible
-        cy.get(':nth-child(1) > .product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').should('exist')
-
-        // Check if the add to cart button is visible
-        cy.get(':nth-child(1) > .product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').should('exist')
-        
-        // Check if the add to cart button is clickable
-        cy.get(':nth-child(1) > .product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').click()
+  
+    it('Checks product hover state and add-to-cart visibility', () => {
+      cy.visit('https://salinaka-ecommerce.web.app')
+      cy.contains('Shop').click()
+  
+      cy.get('.product-card').first().trigger('mouseover')
+  
+      cy.get('.product-card')
+        .first()
+        .find('.product-card-button')
+        .should('be.visible')
+        .click({ force: true })
     })
-
-    it('Checks if a product is clicked, the product page is opened', () => {
-        cy.visit('https://salinaka-ecommerce.web.app')
-
-        // Click Shop
-        cy.contains('Shop').click()
-
-        // Wait for products to load and checks if they are visible
-        cy.get('.product-card', { timeout: 10000 }).should('be.visible')
-
-        // Click on the first product card
-        cy.get(':nth-child(1) > .product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').click()
-
-        // Check if the product page is opened
-        cy.url().should('include', '/product/')
+  
+    it('Checks that clicking a product opens the product detail modal', () => {
+      cy.visit('https://salinaka-ecommerce.web.app')
+      cy.contains('Shop').click()
+  
+      cy.get('.product-card').first().click()
+      cy.url().should('include', '/product/')
     })
-
-    it('Checks if the product page contains the product name, price, and description', () => {
-        cy.visit('https://salinaka-ecommerce.web.app')
-
-        // Click Shop
-        cy.contains('Shop').click()
-
-        // Wait for products to load and checks if they are visible
-        cy.get('.product-card', { timeout: 10000 }).should('be.visible')
-
-        // Click on the first product card
-        cy.get(':nth-child(1) > .product-card > .product-card-content > .product-card-img-wrapper > .product-card-img').click()
-
-        // Check if the product name is visible
-        cy.get('.margin-top-0').should('exist')
-
-        // Check if the product price is visible
-        cy.get('.product-modal-action > .button').should('exist')
-
-        // Check if the product description is visible
-        cy.get('.product-modal-details > :nth-child(4)').should('exist')
-
-        // Check if the product brand is visible
-        cy.get('.product-modal-details > :nth-child(2)').should('exist')
-
-        // Check if the product image is visible
-        cy.get('.product-modal-image').should('exist')
-
-        // Check if the product add to cart button is visible
-        cy.get('.product-modal-action > .button').should('exist')
+  
+    it('Checks product detail modal contents (name, price, brand, image)', () => {
+      cy.visit('https://salinaka-ecommerce.web.app')
+      cy.contains('Shop').click()
+  
+      cy.get('.product-card').first().click()
+  
+      cy.get('.product-modal-details').within(() => {
+        cy.get('.margin-top-0').should('exist') // name
+        cy.get(':nth-child(2)').should('exist') // brand
+        cy.get(':nth-child(4)').should('exist') // description
+      })
+  
+      cy.get('.product-modal-image').should('exist')
+      cy.get('.product-modal-action > .button').should('exist')
     })
-
-
-    })
+  })
+  
